8e82a84cae7bfe50ea60cb36f37e20f3ed8040ed
mrana007 <rana.es.bc@gmail.com>
2023-09-24 06:07:58 +0600

    added job category and filter by remote or onsite

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,8 @@
 const Footer = () => {
   return (
     <footer className="footer p-10 bg-base-200 text-base-content">
-      <aside>
+     <div className="footer max-w-6xl mx-auto">
+     <aside>
         <svg
           width="50"
           height="50"
@@ -39,6 +40,7 @@ const Footer = () => {
         <a className="link link-hover">Privacy policy</a>
         <a className="link link-hover">Cookie policy</a>
       </nav>
+     </div>
     </footer>
   );
 };
